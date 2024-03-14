55207a94cdaf7886bf61883bd45281635db2a98b
Likvik89 <erik@elfisk.dk>
2024-03-14 14:23:51 +0100

    v0.0.0.2

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,8 +1,9 @@
-extends Node2D
+extends RigidBody2D
 
 var waypoints = []
 var waypoint_reached = 0
-var speed = 2000
+var speed = 500
+var maxspeed = 500
 
 func _ready():
 	var tween = get_tree().create_tween()
@@ -11,13 +12,26 @@ func _ready():
 			waypoints.append(waypoint)
 	
 
+func _integrate_forces(state):
+	pass
+	var way = (waypoints[waypoint_reached].position-position).normalized()
+	if linear_velocity.length() <= maxspeed:
+		apply_central_force(way*speed)
 
 func _process(delta):
-	
-	#position = global_position-waypoints[waypoint_reached].global_position
 	pass
+	#print(waypoint_reached)
 
 
 func _on_area_2d_body_entered(waypoint):
 	if waypoint.is_in_group('waypoints'):
 		waypoint_reached += 1
+		
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group('waypoints'):
+		waypoint_reached += 1
+		apply_central_impulse(-linear_velocity.normalized()*speed)
+		var way = (waypoints[waypoint_reached].position-position).normalized()
+		apply_central_impulse(way*speed)

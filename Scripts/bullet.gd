0cc622d6b1b1e0492bf724b4966a0fd921775916
Likvik89 <erik@elfisk.dk>
2024-03-19 10:57:06 +0100

    v0.0.1
    bullet go zoom but good

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,27 +1,23 @@
 extends RigidBody2D
 
 var _position
-var speed = 3000
-
+var speed = 10
 
 var module_spin = false
-var rotationspeed = 2
+var rotationspeed = 1
 
 var module_homing = false
 
 
-
 func _ready():
-	pass
-
-func _physics_process(delta):
 	var direction = (_position-position).normalized()
-	apply_central_impulse(direction)
-	
+	apply_central_impulse(direction*speed)
+
+
 
 func _process(delta):
 	
 	if module_spin:
-		rotation += rotationspeed
+		angular_velocity += rotationspeed
 	print(rotation)
 	

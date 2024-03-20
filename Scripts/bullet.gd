610f56fa78446837f9e943919565a43670b0a77c
Likvik89 <erik@elfisk.dk>
2024-03-20 20:43:34 +0100

    v0.0.2.1
    blade sprite

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -2,9 +2,10 @@ extends RigidBody2D
 
 var _position
 var speed = 100
+var damage = 10
 
 var module_spin = false
-var rotationspeed = 1
+var rotationspeed = 20
 
 var module_homing = false
 var homing_speed = 20
@@ -12,6 +13,9 @@ var homing_speed = 20
 var module_piercing = false
 var pierce_limit = 2
 var current_pierce = 0
+
+var module_blade = false
+
 
 func _ready():
 	var direction = (_position-position).normalized()
@@ -35,7 +39,7 @@ func _process(delta):
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("enemies"):
-		body.take_damage(10)
+		body.take_damage(damage)
 		if module_piercing:
 			current_pierce += 1
 			if current_pierce >= pierce_limit:

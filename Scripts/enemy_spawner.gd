bf717baa2c9eccf3d21975bb46a252ccf839471b
Likvik89 <erik@elfisk.dk>
2024-04-10 12:19:57 +0200

    v0.2
    enemy wave

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -3,50 +3,40 @@ var waypoints = []
 @export var slime : PackedScene
 var spawnrate = 0
 var current_wave = 0
-const last_wave = 9
-
-var waves = [
-	{ "type" : "pause", "amount": 6},
-	{ "type":"wave", "enemies" : [
-		{"kind" : "slime", "amount" : 5},
-		{"kind" : "warrior", "amount" : 1}
-	]},
-	{ }
-			]
 
 var wave1 = [
-	{"type" : "pause", "time" : 6},
-	{"type" : "enemies" , "kind" : "slime"}
+	{"type" : "pause", "time" : 2},
+	{"type" : "enemy" , "kind" : "slime"},
+	{"type" : "pause", "time" : 4},
+	{"type" : "enemy" , "kind" : "slime"},
+	{"type" : "pause", "time" : 2},
+	{"type" : "enemy" , "kind" : "slime"}
 ]
+var waves = [wave1]
 
 func _ready():
-	
-	print(wave1)
 	for waypoint in $"../waypoints".get_children():
 		if waypoint.is_in_group('waypoints'):
 			waypoints.append(waypoint)
 
-func spawn():
-	var enemy = slime.instantiate()
+func spawn(enmy):
+	var enemy
+	if enmy == "slime":
+		enemy = slime.instantiate()
 	enemy.waypoints = waypoints
 	enemy.global_position = $SpawnPoint.global_position
 	add_child(enemy)
 	
 
 func _process(delta):
-	var a = wave1.pop_front()
-	if a:
-		if a.type == "pause":
-			print(a.time)
-		elif a.type == "enemies":
-			if a.kind == "slime":
-				
-				print(a.kind)
-	
+	var wave = waves[current_wave]
 	
 	if spawnrate <= 0:
-		spawn()
-		spawnrate = 2.5
+		var next = wave.pop_front()
+		if next.type == "pause":
+			spawnrate = next.time
+		
+		elif next.type == "enemy":
+			spawn(next.kind)
 	else:
 		spawnrate -= delta
-

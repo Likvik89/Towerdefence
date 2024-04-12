bc2cc1df29c0625f5b7577fd6ada55c5fa9f994c
Likvik89 <erik@elfisk.dk>
2024-04-12 10:23:37 +0200

    v0.6.2
    background

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -6,15 +6,15 @@ func _integrate_forces(state):
 	pass
 
 func _process(delta):
-	#if Input.is_action_pressed("move_up"):
-	#	linear_velocity = Vector2.UP*speed
-	#elif Input.is_action_pressed("move_down"):
-	#	linear_velocity = Vector2.DOWN*speed
-	#elif Input.is_action_pressed("move_left"):
-	#	linear_velocity = Vector2.LEFT*speed
-	#elif Input.is_action_pressed("move_right"):
-	#	linear_velocity = Vector2.RIGHT*speed
-	#
-	#else:
-	#	linear_velocity = Vector2(0,0)
+	if Input.is_action_pressed("move_up"):
+		linear_velocity = Vector2.UP*speed
+	elif Input.is_action_pressed("move_down"):
+		linear_velocity = Vector2.DOWN*speed
+	elif Input.is_action_pressed("move_left"):
+		linear_velocity = Vector2.LEFT*speed
+	elif Input.is_action_pressed("move_right"):
+		linear_velocity = Vector2.RIGHT*speed
+	
+	else:
+		linear_velocity = Vector2(0,0)
 	pass

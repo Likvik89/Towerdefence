13423f59eabb7af4024c5772076301e10337a1c2
Likvik89 <erik@elfisk.dk>
2024-04-22 07:58:28 +0200

    v0.7.7
    smoll slime be schmoving

--- Scripts/slime_smoll.gd
+++ Scripts/slime_smoll.gd
@@ -3,9 +3,15 @@ extends RigidBody2D
 
 var waypoints = []
 var waypoint_reached
-var speed
+
+var speed = 1
+var acceleration = 2
+var maxspeed = 50
+
+var pushed = false
+
 var bounty
-var maxspeed = 250
+
 var health
 var alive = true
 var maxhealth
@@ -15,17 +21,8 @@ func _ready():
 	health = maxhealth
 	waypoints = GlobalInfo.waypoints
 	GlobalInfo.enemies_left.append(self)
-	var way = (waypoints[waypoint_reached].position-position).normalized()
-	apply_central_force(way*speed*3)
+	
 
-
-func _integrate_forces(state):
-	var way = (waypoints[waypoint_reached].position-position).normalized()
-	if linear_velocity.length() <= maxspeed:
-		apply_central_force(way*speed)
-		
-	#else:
-	#	linear_velocity = Vector2(0,0)
 
 func take_damage(damage):
 	health -= damage
@@ -38,7 +35,13 @@ func _process(delta):
 		GlobalInfo.money += bounty
 		GlobalInfo.enemies_left.erase(self)
 		queue_free()
-	
+	var way = (waypoints[waypoint_reached].position-position).normalized()
+	if not pushed:
+		linear_velocity = Vector2(way*speed)
+		if speed < maxspeed:
+			speed = linear_velocity.length()
+			speed += acceleration
+	print(speed)
 
 
 func _on_area_2d_area_entered(area):
@@ -49,6 +52,4 @@ func _on_area_2d_area_entered(area):
 			queue_free()
 		else:
 			waypoint_reached += 1
-			linear_velocity = Vector2(0,0)
-			var way = (waypoints[waypoint_reached].position-position).normalized()
-			apply_central_impulse(way*speed*3)
+			
